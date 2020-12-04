a3481d96d48fa232a5aded3f5ca4f79d958f4cb7
reuschi <quattro04@gmx.de>
2020-12-05 00:19:10 +0100

    frame logic changed

--- frame.py
+++ frame.py
@@ -15,6 +15,19 @@ def getFiles():
     return images
 
 
+def exitSlideshow():
+    os.system("sudo pkill fbi")
+
+
+def runSlideshow():
+    exitSlideshow()
+    bashCommand = "sudo fbi --noverbose -a -t 7 /home/pi/python/smart-photo-frame/images/*.jpg -T 1"
+    os.system(bashCommand)
+
+
+
+
+
 #api_id = 2640738
 #api_hash = "1a909a049af457fff5e5cffdd4524199"
 
@@ -31,15 +44,17 @@ def getFiles():
 if __name__ == '__main__':
     #images = getFiles()
 
+    runSlideshow()
     #for file in images:
-    bashCommand = "sudo fbi --noverbose -a -t 7 /home/pi/python/smart-photo-frame/images/*.jpg -T 1"
-    os.system(bashCommand)
+#    bashCommand = "sudo fbi --noverbose -a -t 7 /home/pi/python/smart-photo-frame/images/*.jpg -T 1"
+#    os.system(bashCommand)
     #    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     #    output, error = process.communicate()
-    time.sleep(20)
-    kill = os.system("pgrep fbi")
-    print("fbi process number: {}".format(kill))
-    os.system("sudo pkill fbi")
+    time.sleep(25)
+    exitSlideshow()
+#    kill = os.system("pgrep fbi")
+#    print("fbi process number: {}".format(kill))
+#    os.system("sudo pkill fbi")
 
 #    proc1 = subprocess.Popen(["sudo", "fbi", "--noverbose", "-a", "-t", "7", "./images/*.jpg", "-T", "1"])
 #    print("Process with ID {} is running".format(proc1.pid))
