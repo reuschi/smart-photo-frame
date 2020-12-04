7a2aefc845b079dceddcfa4cab52df83ca3b5a96
reuschi <quattro04@gmx.de>
2020-12-04 20:34:29 +0100

    fram logic added

---
+++ frame.py
@@ -0,0 +1,29 @@
+
+from pyrogram import Client, filters
+import os
+import subprocess
+import time
+
+
+
+
+#api_id = 2640738
+#api_hash = "1a909a049af457fff5e5cffdd4524199"
+
+
+#app = Client("my_account")
+#app.run()
+
+#@app.on_message(filters.private)
+#async def hello(client, message):
+#    await message.reply_text(f"Hello {message.from_user.mention}")
+
+
+
+if __name__ == '__main__':
+#    app.run()
+    proc1 = subprocess.Popen("sudo fbi -a -t 5 ./images/*.jpg -T 1")
+    print("Process with ID {} is running".format(proc1.pid))
+    time.sleep(10)
+    proc1.terminate()
+    print("Process terminated")
