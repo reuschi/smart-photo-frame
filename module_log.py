59e78d63c9f8fdb630a7da00f874e99566ff9cc7
reuschi <quattro04@gmx.de>
2020-12-13 15:39:06 +0100

    Logging adjusted

--- module_log.py
+++ module_log.py
@@ -14,7 +14,7 @@ def log_To_File(logging):
     logging_path = "/home/pi/python/smart-photo-frame/" + "message.log"
     logfile = open(logging_path, "a")
     datetime = time.strftime("%b %d %Y %H:%M:%S")
-    log = datetime + " " + logging + "\n"
+    log = str(datetime) + " " + str(logging) + "\n"
     logfile.write(log)
     logfile.close()
 
