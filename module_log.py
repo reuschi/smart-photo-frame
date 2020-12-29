41065622f34412892fd417191e6e0f6aa2d944fa
reuschi <quattro04@gmx.de>
2020-12-30 00:54:33 +0100

    Logging adjustments

--- module_log.py
+++ module_log.py
@@ -9,8 +9,7 @@ logging_path = pathlib.Path(pathlib.Path(__file__).parent.absolute() / "message.
 
 
 def log_To_Screen(logging, **kwargs):
-    datetime = time.strftime("%b %d %H:%M:%S")
-    print(f"{datetime} - {logging}", flush=True)
+    print(f"{logging}", flush=True)
 
 
 def log_To_File(logging):
