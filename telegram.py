7ee2e1a4b2a43ddc3082c796c23a567709f06ecf
reuschi <quattro04@gmx.de>
2021-01-11 19:05:48 +0100

    debugging enhancement for deleting images

--- telegram.py
+++ telegram.py
@@ -307,8 +307,9 @@ def main():
                                     reply = subprocess.Popen(bashCommand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                                     stdout, stderr = reply.communicate()
                                     # os.system(f"sudo rm /home/pi/python/smart-photo-frame/images/{img}")
+                                    print(stderr)
+                                    print(stdout)
                                     if stderr is "b''":
-                                        print(stderr)
                                         send_Message(from_id, f"{img} erfolgreich gelöscht")
                                         module_log.log(f"{img} deleted.")
                                         success = True
