ef771e5d514e599219f4bf409ae4debdf47dc1c9
reuschi <quattro04@gmx.de>
2021-12-16 11:02:00 +0100

    Code cleanup

--- texts.py
+++ texts.py
@@ -1,32 +1,5 @@
 
 
-de_telegram = {
-    "return_400": "400 - Fehlerhafte Anfrage!",
-    "return_401": "401 - Nicht authorisiert!",
-    "return_403": "403 - Ausführung verboten!",
-    "return_404": "404 - Anfrage nicht gefunden",
-    "return_406": "406 - Anfrage nicht ordnungsgemäß",
-    "return_420": "420 - The maximum allowed number of attempts to invoke the given method with the given input parameters has been exceeded.",
-    "return_500": "500 - An internal server error occurred while a request was being processed.",
-    "thanks_image_upload": "Danke für das Bild. Ich habe es für die Verwendung in der Datenbank gespeichert und die Präsentation neu gestartet.",
-    "sender_not_allowed": "Sie sind nicht berechtigt!",
-    "extension_not_allowed": "Die Dateierweiterung ist nicht erlaubt!",
-    "a": "b"
-}
-
-en_telegram = {
-    "return_400": "400 - Bad Request!",
-    "return_401": "401 - You're not authorized!",
-    "return_403": "403 - Action is Forbidden!",
-    "return_404": "404 - Request not found",
-    "return_406": "406 - Request not acceptable",
-    "return_420": "420 - The maximum allowed number of attempts to invoke the given method with the given input parameters has been exceeded.",
-    "return_500": "500 - An internal server error occurred while a request was being processed",
-    "thanks_image_upload": "Danke für das Bild. Ich habe es für die Verwendung in der Datenbank gespeichert und die Präsentation neu gestartet.",
-    "sender_not_allowed": "Sender not allowed to send photos",
-    "a": "b"
-}
-
 texts = {
     "DE": {
         "tg": {
