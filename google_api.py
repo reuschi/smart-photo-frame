7308fa55a77a7437ff8030bbe234cb8a223b31a4
reuschi <quattro04@gmx.de>
2022-12-27 02:41:03 +0100

    General module adjustments after first test

--- google_api.py
+++ google_api.py
@@ -60,6 +60,7 @@ class Gmail:
 
         with build("gmail", "v1", credentials=self.creds) as service:
             messages = service.users().messages().list(userId="me").execute()
+
         return messages
 
     def read_message(self, message_id):
@@ -67,6 +68,7 @@ class Gmail:
 
         with build("gmail", "v1", credentials=self.creds) as service:
             message = service.users().messages().get(userId="me", id=message_id).execute()
+
         return message
 
     def delete_message(self, message_id):
@@ -74,17 +76,17 @@ class Gmail:
 
         with build("gmail", "v1", credentials=self.creds) as service:
             result = service.users().messages().trash(userId="me", id=message_id).execute()
+
         return result
 
     def read_attachment(self, message_id: str, attachment_id: str, filename: str,
                         destination: str = "images"):
         """ Read an attachment of a message and write it to the local file system """
 
-        #store_dir = os.getcwd()
         store_dir = Path(__file__).parent.absolute()
-        filename = "mail_" + time.strftime("%Y%m%d_") + filename
+        extension = filename.split(".")[1]
+        filename = "mail_" + time.strftime("%Y%m%d_") + filename + "." + extension.lower()
         path = Path(store_dir / destination / filename)
-        # print(filename)
 
         with build("gmail", "v1", credentials=self.creds) as service:
             attachment = service.users().messages().attachments().get(
@@ -93,7 +95,7 @@ class Gmail:
             file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
 
             if path.exists():
-                module_log.log("Datei existiert bereits")
+                module_log.log(f"Datei '{filename}' existiert bereits")
 
             else:
                 with open(path, 'wb') as file:
@@ -107,37 +109,38 @@ class Gmail:
 
         success = False
 
-        for mail in mails['messages']:
-            #print(mail['id'])
-            message = self.read_message(mail['id'])
-            message_id = message["id"]
-            message_parts = message["payload"]["parts"]
+        try:
+            for mail in mails['messages']:
+                #print(mail['id'])
+                message = self.read_message(mail['id'])
+                message_id = message["id"]
+                message_parts = message["payload"]["parts"]
 
-            for part in message_parts:
-                #print(f"Part-ID: {part['partId']}")
-                #print(f"Mime-Type: {part['mimeType']}")
-                #print(f"Filename: {part['filename']}")
+                for part in message_parts:
+                    #print(f"Part-ID: {part['partId']}")
+                    #print(f"Mime-Type: {part['mimeType']}")
+                    #print(f"Filename: {part['filename']}")
 
-                extension = part['filename'].split(".")
-                #extension = extension[1].lower()
-                #print(extension)
+                    extension = part['filename'].split(".")
 
-                if 'image' in part['mimeType'] and extension[1].lower() in static.file_extensions:
-                    #print(f"Attachment-ID: {part['body']['attachmentId']}")
-                    if self.read_attachment(message_id, part['body']['attachmentId'],
-                                            part['filename']):
-                        module_log.log(f"{part['filename']} erfolgreich heruntergeladen!")
-                        success = True
-                        #print(self.delete_message(message_id))
+                    if 'image' in part['mimeType'] and extension[1].lower() in static.file_extensions:
+                        #print(f"Attachment-ID: {part['body']['attachmentId']}")
+                        if self.read_attachment(message_id, part['body']['attachmentId'],
+                                                part['filename']):
+                            module_log.log(f"{part['filename']} erfolgreich heruntergeladen!")
+                            success = True
+                        else:
+                            module_log.log(f"Download von {part['filename']} fehlgeschlagen!")
+                            success = False
                     else:
-                        module_log.log(f"Download von {part['filename']} fehlgeschlagen!")
-                        success = False
-                else:
-                    continue
+                        continue
 
-            if success:
-                #module_log.log(self.delete_message(message_id))
-                module_log.log(f"Mail {mail['id']} löschen")
-                success = False
+                if success:
+                    #module_log.log(self.delete_message(message_id))
+                    module_log.log(f"Mail {mail['id']} löschen")
+                    success = False
+
+        except Exception as exc:
+            module_log.log(f"Exception during file download from mail: {exc}")
 
         return success
