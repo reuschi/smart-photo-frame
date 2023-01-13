4b61d2ce5cde0763f1fa5aaee373b3c0eae7a87e
reuschi <quattro04@gmx.de>
2023-01-13 16:56:40 +0100

    Refactoring module

--- imap.py
+++ imap.py
@@ -4,6 +4,7 @@ from imaplib import IMAP4_SSL
 from socket import gaierror
 import email
 import email.header
+import time
 from pathlib import Path
 
 import module_log
@@ -19,9 +20,71 @@ class ImapMail:
         self.email_account = account
         self.email_password = passwd
         self.hostname = hostname
-        self.allowed_extensions = ext
+        self.allowed_extensions = static.file_extensions
         self.subfolder = subfolder
         self.language = static.language
+        self.imap = None
+
+    def get_filename(self, part):
+        """ Get filename and extension of the downloadable file """
+
+        if part.get_filename():
+            #print(time.time_ns())
+            cur_time = str(int(time.time())) + "_"
+            file_name = "mail_" + cur_time + part.get_filename()
+            file_extension = Path(file_name).suffix.replace('.', '').lower()
+            module_log.log(f"File Extension: {file_extension}")
+
+            file = {
+                'name': file_name,
+                'extension': file_extension
+            }
+        else:
+            file = None
+
+        return file
+
+    def get_attachment(self, part, directory, success):
+        """ Get attachment of mail and write it into the correct directory """
+
+        # Get filename and extension of the downloadable file
+        file = self.get_filename(part)
+
+        # Only download file if its extension is in config file
+        if file and (file['extension'] in self.allowed_extensions):
+            # Define path where to store the file locally
+            file_path = Path(Path(__file__).parent.absolute() / directory /
+                             file['name'].lower())
+
+            if not Path.is_file(file_path):
+                # If file is not yet downloaded
+                with open(file_path, 'wb') as file:
+                    file.write(part.get_payload(decode=True))
+                module_log.log(f"New file downloaded: {file_path}")
+                success = True
+            elif Path.is_file(file_path):
+                # If file already exists, don't download it
+                module_log.log("Filename already exists!")
+            else:
+                # If there is no new file to download
+                module_log.log("No new file downloaded!")
+        else:
+            # If file extension is not allowed to download
+            module_log.log(f"File Extension not allowed ('{file['extension']}')")
+
+        return success
+
+    def delete_message(self, mail, num):
+        """ Delete mail from inbox """
+
+        try:
+            if "gmail.com" in self.hostname:
+                mail.store(num, '+X-GM-LABELS', '\\Trash')
+                #mail.expunge()
+            else:
+                mail.store(num, '+FLAGS', '\\Deleted')
+        except Exception as exc:
+            module_log.log(f"Exception while delete: {exc}")
 
     def download_attachment(self, mail, directory: str = "images"):
         """ Download attachments from mails stored in a specific sub folder """
@@ -32,70 +95,67 @@ class ImapMail:
             module_log.log("Request to Mailbox was not successful!")
             return False
 
+        # Check if there is a new mail
         if not data[0]:
             module_log.log("No new mail found")
-        else:
-            for num in data[0].split():
-                module_log.log("Trying to download mail attachment...")
+            return False
 
-                # Try to fetch new mails
-                receive, data = mail.fetch(num, '(RFC822)')
-                if receive != 'OK':
-                    module_log.log("ERROR getting message")
-                    return False
+        # Run through new mails
+        for num in data[0].split():
+            module_log.log("Trying to fetch new mail...")
 
-                msg = email.message_from_bytes(data[0][1])
+            # Try to fetch new mails
+            receive, data = mail.fetch(num, '(RFC822)')
 
-                # Downloading attachments
-                for part in msg.walk():
-                    # Only download attachments, if they are real attachments
-                    if part.get_content_maintype() == 'multipart':
-                        continue
+            if receive != 'OK':
+                module_log.log("ERROR getting message")
+                return False
 
+            msg = email.message_from_bytes(data[0][1])
+
+            # Downloading attachments
+            for part in msg.walk():
+                if part.get_content_maintype() == 'multipart' or \
+                        part.get('Content-Disposition') is None:
                     module_log.log("No attachment found!")
+                    continue
 
-                    if part.get('Content-Disposition') is None:
-                        continue
+                #if part.get('Content-Disposition') is None:
+                #    module_log.log("No attachment found!")
+                #    continue
 
-                    # Get filename and extension of the downloadable file
-                    file_name = "mail_" + part.get_filename()
-                    file_extension = Path(file_name).suffix.replace('.','').lower()
-                    module_log.log(f"File Extension: {file_extension}")
+                if static.debug:
+                    module_log.log(f"Content-Type: {part.get_content_type()}")
+                # Only download attachments, if they are real attachments
+                if "image" in part.get_content_type():
+                    module_log.log("Image in attachment found")
+                    success = self.get_attachment(part, directory, success)
 
-                    # Only download file if its extension is in config file
-                    if bool(file_name) and (file_extension in self.allowed_extensions):
-                        # Define path where to store the file locally
-                        file_path = Path(Path(__file__).parent.absolute() /
-                                                 directory / file_name.lower())
+            # After downloading the attachments move the mail into Trash folder
+            if receive == 'OK':
+                self.delete_message(mail, num)
 
-                        if not Path.is_file(file_path):
-                            # If file is not yet downloaded
-                            with open(file_path, 'wb') as file:
-                                file.write(part.get_payload(decode=True))
-                            module_log.log(f"New file downloaded: {file_path}")
-                            success = True
-                        elif Path.is_file(file_path):
-                            # If file already exists, don't download it
-                            module_log.log("Filename already exists!")
-                        else:
-                            # If there is no new file to download
-                            module_log.log("No new file downloaded!")
-                    else:
-                        # If file extension is not allowed to download
-                        module_log.log(f"File Extension not allowed ('{file_extension}')")
-
-                # After downloading the attachments move the mail into Trash folder
-                try:
-                    if receive == 'OK':
-                        if "gmail.com" in self.hostname:
-                            mail.store(num, '+X-GM-LABELS', '\\Trash')
-                            mail.expunge()
-                        else:
-                            mail.store(num, '+FLAGS', '\\Deleted')
-                except Exception as exc:
-                    module_log.log(exc)
+                if static.debug:
+                    module_log.log(f"{num} deleted")
 
         return success
+
+    def imap_login(self):
+        """ Login to imap account """
+
+        self.imap = IMAP4_SSL(host=self.hostname, port=993)
+        _receive, data = self.imap.login(self.email_account, self.email_password)
+
+        # For debugging purposes
+        if static.debug:
+            module_log.log(data)
+
+    def imap_close_connection(self, ):
+        """ Close connection to mail server """
+
+        self.imap.expunge()
+        self.imap.close()
+        self.imap.logout()
 
     def init_imap(self):
         """ Receive new mails """
@@ -105,18 +165,13 @@ class ImapMail:
         try:
             module_log.log("Trying to fetch new mails")
             # Initialize connection and login to mail account
-            imap = IMAP4_SSL(host=self.hostname, port=993)
-            receive, data = imap.login(self.email_account, self.email_password)
-
-            # For debugging purposes
-            if static.debug:
-                module_log.log(data)
+            self.imap_login()
 
             # Receive Mailboxes
-            receive, mailboxes = imap.list()
+            receive, mailboxes = self.imap.list()
             if receive == 'OK':
-                # For debugging purposes
                 module_log.log("Mailbox found")
+
                 if static.debug:
                     # For debugging purposes
                     module_log.log(str(mailboxes))
@@ -125,17 +180,16 @@ class ImapMail:
                 return texts.texts[self.language]['imap']['no_mailbox_found']
 
             # Select mailbox folder to download images from and download new images
-            receive, data = imap.select(f'"{self.subfolder}"')
+            receive, _data = self.imap.select(f'"{self.subfolder}"')
             if receive == 'OK':
                 module_log.log("Processing mailbox...")
 
-                success = self.download_attachment(imap)
+                success = self.download_attachment(self.imap)
             else:
                 return texts.texts[self.language]['imap']['mailbox_open_error'].format(receive)
 
             # Close connection to mail server
-            imap.close()
-            imap.logout()
+            self.imap_close_connection()
 
         except gaierror:
             module_log.log("DNS name not resolvable. Try again later.")
